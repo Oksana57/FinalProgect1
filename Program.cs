ddc71ac8638021521e6b7a567e224d05a02d46b2
Oksana Blokhina <oksa-blokhi@yandex.ru>
2022-12-03 17:21:19 +0300

    Fillarray

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// Написать программу, которая из имеющегося массива 
+// строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+
+// string[] arg = {"Hello", "2", "wotld", ":-"};
+
+// void Array (string[]arg)
+// {
+//     string[]A = new string [size];
+
+// }
+
+
+string[] AS;
+int size = 4;
+string a;
+
+AS = new string[size];
+
+Console.WriteLine("Введите массив:");
+
+      for (int i=0; i<AS.Length; i++)
+      {
+        Console.Write("AS[{0}] = ", i);
+        AS[i] = Console.ReadLine();
+        // Console.WriteLine($"{AS[i]}");
+      }
+       Console.WriteLine($"{AS}");
+
